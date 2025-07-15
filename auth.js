ff31bb539a7af1bf85abd6d83eabb6cae15ed0c9
Cem <gizlimustafa740@gmail.com>
2025-07-15 21:06:21 +0300

    cme

---
+++ auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// Kayıt
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(409).json({ message: "Bu e-posta zaten kayıtlı." });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "Kayıt başarılı" });
+  } catch (err) {
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+});
